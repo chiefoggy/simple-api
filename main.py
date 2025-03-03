eb65a7b8b5a208ecc9373979154d815888183535
chiefoggy <81455726+chiefoggy@users.noreply.github.com>
2025-03-03 17:44:14 +0800

    added new API endpoint to obtain random user data

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ def get_random_fact():
         return jsonify(response.json())
     else:
         return jsonify({"error": "Failed to fetch fact"}), response.status_code
+    
+# GET request to fetch particulars of a random user data
+@app.route('/random-user', methods=['GET'])
+def get_random_user():
+    url = "https://randomuser.me/api/"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch random user data"}), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
